219c62212d69873d71ff3af260d4496db29a2ddb
GamboaBrayan <118092973+GamboaBrayan@users.noreply.github.com>
2026-09-15 21:04:09 -0500

    feat: add landing page components, layouts, and GSAP animation vendor scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://monniagency.com'
+  site: 'https://monniagency.com',
+  devToolbar: {
+    enabled: false
+  }
 });
